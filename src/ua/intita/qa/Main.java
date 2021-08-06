5e10cbea32df65c04413c319797c891db2639cf6
Anna <anna.kozachko@gmail.com>
2021-08-07 00:42:27 +0300

    Created ShopApp application

--- src/ua/intita/qa/Main.java
+++ src/ua/intita/qa/Main.java
@@ -1,13 +1,22 @@
 package ua.intita.qa;
 
+import java.util.Scanner;
+
 public class Main {
-    public void main(String[] args) {
-        Product[] prod = new Product[6];
+    public static void main(String[] args) {
+       Product[] prod = new Product[6];
+
         prod[0] = new Product(1, "cake", 12345678, "Roshen", 300, 3, 1);
         prod[1] = new Product(2, "candy", 33455656, "Svitoch", 200, 60, 5);
-        prod[2] = new Product(3, "biscuit", 12435678, "Svitoch", 150, 4, 1);
+        prod[2] = new Product(3, "cake", 12435678, "Svitoch", 150, 4, 1);
         prod[3] = new Product(4, "lollipops", 12789678, "Konti", 120, 70, 1);
-        prod[4] = new Product(5, "marshmallow", 14345678, "Solodka Mriya", 240, 5, 6);
-        prod[5] = new Product(6, "chocolate", 12333678, "Roshen", 300, 140, 1);
+        prod[4] = new Product(5, "cake", 14345678, "Solodka Mriya", 240, 5, 6);
+        prod[5] = new Product(6, "lollipops", 12333678, "Roshen", 300, 140, 1);
+        Shop shop = new Shop(prod);
+        shop.getProductsByName("cake");
+        shop.getProductsByName("marshmallow");
+        shop.getProductsByNamePrice("cake",prod,300);
+        shop.getProductsByNameStorage("lollipops", 70);
     }
+
 }
--- src/ua/intita/qa/Shop.java
+++ src/ua/intita/qa/Shop.java
@@ -11,7 +11,22 @@ public class Shop {
     public void getProductsByName(String name) {
         for (Product product : products) {
             if (product.getName().equals(name)) {
-                System.out.println(product.getName());
+                System.out.println(product.toString());
+            }
+        }
+    }
+
+    public void getProductsByNamePrice(String name, Product[] prod, int price) {
+        for (Product product : products) {
+            if (product.getName().equals(name) && product.getPrice()<=price ) {
+                System.out.println(product.toString());
+            }
+        }
+    }
+    public void getProductsByNameStorage(String name, int storage) {
+        for (Product product : products) {
+            if (product.getName().equals(name) && product.getStorage()>=storage ) {
+                System.out.println(product.toString());
             }
         }
     }
